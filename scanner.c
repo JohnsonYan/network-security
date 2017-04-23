9bca318bbc85f8ce0224235163ebac08bf4aea19
johnson <719162522@qq.com>
2017-04-23 23:34:49 +0800

    finish hostscan, portscan has some bug

--- scanner.c
+++ scanner.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
@@ -13,9 +14,9 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-#define IPLEN 16
 #define PACKET_SIZE 4096
-#define TIME_OUT_TIME 3
+#define IPLEN 16
+
 unsigned short cal_chksum(unsigned short *addr,int len)
 {
     int sum=0;
@@ -40,71 +41,39 @@ unsigned short cal_chksum(unsigned short *addr,int len)
     answer = ~sum;             /* 注意类型转换，现在的校验和为16位 */
     return answer;
 }
-char* splitIP(char *host, char *ip)
-{
-    int i;
-    // strtok会修改host，所以新建一个变量来防止这种情况发生
-    char buf[IPLEN];
-    char *pbuf=buf;
-    strcpy(pbuf,host);
-    char *token = NULL;
-    char *save = NULL;
-    token = strtok_r(pbuf, ".",&save);
-    strcat(ip, token);
-    for(i = 0; i < 2; i++)
-    {
-        token = strtok_r(NULL, ".",&save);
-        strcat(ip, ".");
-        strcat(ip, token);
-    }
-    strcat(ip, ".");
-    return ip;
-}
 
 int portScan(char *host)
 {
     struct sockaddr_in sa;
     int sockfd, status;
-    int i,j;
-    char ip[IPLEN],*ip_ptr;
-    char ip_number[3];
-    for(i=2; i < 256; i++)
-    {
-        memset(ip, '\0', IPLEN);
-        memset(ip_number, '\0', 3);
-        ip_ptr = ip;
-        // 分割ip地址
-        ip_ptr = splitIP(host, ip);
-        sprintf(ip_number, "%d", i);
-        strcat(ip_ptr, ip_number);
-        printf("222 %s\n", ip_ptr);
-        for(j=1; j < 1024; j++) {
-            // 建立套接字
-            sockfd = socket(AF_INET, SOCK_STREAM, 0);
-            bzero(&sa, sizeof(sa));
-            sa.sin_family = AF_INET;
-            inet_pton(AF_INET, ip_ptr, &sa.sin_addr);
-            sa.sin_port = htons(j);
-            // 尝试与目标主机和端口i建立连接
-            status = connect(sockfd, (struct sockaddr *)&sa, sizeof(sa));
-            if(status == -1) {
-                // 如果连接失败
-                close(sockfd);
-                continue;
-            }
-            // 连接成功
-            printf("connected %s:%d\n",ip_ptr,j);
+    int i;
+    for(i=1; i < 65536; i++) {
+        // 建立套接字
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        bzero(&sa, sizeof(sa));
+        sa.sin_family = AF_INET;
+        inet_pton(AF_INET, host, &sa.sin_addr);
+        sa.sin_port = htons(i);
+        // 尝试与目标主机和端口i建立连接
+        status = connect(sockfd, (struct sockaddr *)&sa, sizeof(sa));
+        if(status == -1) {
+            // 如果连接失败
             close(sockfd);
+            continue;
         }
+        // 连接成功
+        printf("connected %s:%d\n",host,i);
+        close(sockfd);
     }
     return 0;
 }
 
-int hostscan(char *ip) {
+int hostscan(char *ip)
+{
     char sendpacket[PACKET_SIZE];
     char recvpacket[PACKET_SIZE];
     pid_t pid;
-    int datelen = 56; // ICMP数据包中数据的长度
+    int datalen = 56; // ICMP数据包中数据的长度
     struct protoent *protocol;
     protocol = getprotobyname("icmp");
     int sockfd;
@@ -136,7 +105,7 @@ int hostscan(char *ip) {
     if(sendto(sockfd, sendpacket, packsize, 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0)
         perror("sendto error");
 
-    printf("send %d, send done\n",1);
+    //printf("send %d, send done\n",1);
     int n;
     struct sockaddr_in from;
     int fromlen = sizeof(from);
@@ -166,15 +135,46 @@ int hostscan(char *ip) {
 
 }
 
+int hostsScan(char *ip)
+{
+    int i;
+    char buf[IPLEN],*dest_ip = NULL; //buf为ip的备份，strcat会修改ip，导致无法复用
+    char ip_number[3];
+    for(i = 1; i < 256; i++)
+    {
+        strcpy(buf, ip);
+        memset(ip_number, '\0', 3);
+        dest_ip = buf;
+        // 拼接ip地址
+        sprintf(ip_number, "%d", i);
+        strcat(dest_ip, ip_number);
+        printf("%s ------------ ", dest_ip);
+        hostscan(dest_ip);
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    if(argc != 2) {
-        printf("usage: %s ip\n", argv[0]);
+    char *host = NULL;
+    if(argc != 3) {
+        printf("usage: %s [--hostscan|--portscan] [IPaddress]\n", argv[0]);
         exit(1);
     }
-    char *host = NULL;
-    host = argv[1];
-    printf("Destination IP-address:%s\n", host);
-    portScan(host);
-    return 0;
+    if(strcmp(argv[1], "--hostscan") == 0)
+    {
+        host = argv[2];
+        hostsScan(host);
+        return 0;
+    } else if(strcmp(argv[1], "--portscan") == 0)
+    {
+        host = argv[2];
+        printf("Destination IP-address:%s\n", host);
+        portScan(host);
+        return 0;
+    } else
+    {
+        printf("invalid command\n");
+        return 1;
+    }
 }
